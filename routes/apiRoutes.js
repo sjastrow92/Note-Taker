b10bcc422095806071590fd60d057abc2a63bff3
sjastrow <skjastrow@gmail.com>
2024-05-04 15:16:26 -0400

    Added get and post routes for user notes in apiRoutes file.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.get("/notes", (req,res) => {
+router.get('/notes', (req,res) => {
 // get from db.json
+res.sendFile(path.join(__dirname, '../db/db.json'));
 });
+
+  router.post('/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    // creating body for note
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      // creating unique id for each note
+      id: uniqid(),
+    };
+    // pushing created note to be written in the db.json file
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+  });
 
 
 
