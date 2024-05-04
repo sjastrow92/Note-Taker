1912d0b84f5b33551ec093009d717bfa64e79e0c
sjastrow <skjastrow@gmail.com>
2024-05-04 15:54:59 -0400

    added to the readme file.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Note Taker Starter Code
+- The goal of this project is to create an application that allows users to write notes and organize to keep track of tasks.
+
+# Description
+- Allows users to write notes.
+- This app was built using node js with npm inquirer package.
+- Many challenges were faced in the creation of this project. Getting used to the simplified structure of node js while creating and api fetch and post was difficult to string together. 
+- The file structure, paired with many different js files for specificity created a lot fo consufion when making sure eveything was synced correctly and also having each function and groups of code being placed in the correct spot.
+
+# Install
+- To use this app, open in your default browser and click the "get started" button. There you will be able to create notes and see them displayed on the "notes" landing page. See screen shots below.
+
+
+
+# Credits
+- Starter code was provided.
+- Files 18, 20, and our student mini project were used as code reference for bpoth routes files and the server.js.
+- I had a tuturing session with JayTee. He was able to help me write code on server.js as well as make sure my installations were correct.
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,27 @@
-const router = require('express').Router();
-const path = require('path');
+const router = require("express").Router();
+const path = require("path");
+const fs = require('fs');
 
-router.get('/notes', (req,res) => {
-// get from db.json
-res.sendFile(path.join(__dirname, '../db/db.json'));
+router.get("api/notes", (req, res) => {
+  // get from db.json
+  res.sendFile(path.join(__dirname, "../db/db.json"));
 });
 
-  router.post('/notes', (req, res) => {
-    let db = fs.readFileSync('db/db.json');
-    db = JSON.parse(db);
-    res.json(db);
-    // creating body for note
-    let userNote = {
-      title: req.body.title,
-      text: req.body.text,
-      // creating unique id for each note
-      id: uniqid(),
-    };
-    // pushing created note to be written in the db.json file
-    db.push(userNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.json(db);
-  });
-
-
+router.post("api/notes", (req, res) => {
+  let db = fs.readFileSync("db/db.json");
+  db = JSON.parse(db);
+  res.json(db);
+  // creating body for note
+  let userNote = {
+    title: req.body.title,
+    text: req.body.text,
+    // creating unique id for each note
+    id: uniqid(),
+  };
+  // pushing created note to be written in the db.json file
+  db.push(userNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(db));
+  res.json(db);
+});
 
 module.exports = router;
